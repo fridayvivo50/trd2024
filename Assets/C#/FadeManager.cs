b9104352a299be6cf880aa69f7097a1fe081955a
fridayvivo50 <143174679+fridayvivo50@users.noreply.github.com>
2024-10-24 18:00:51 +0800

    人物动画机
    没找到把多个PSD导入一个动画机的方法手搓了一个（

---
+++ Assets/C#/FadeManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeManager : MonoBehaviour
+{
+    public static FadeManager InstanceF;
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public void FadeIn()
+    {
+        myAnimator.SetTrigger("FadeIn");
+    }
+
+    public void FadeOut()
+    {
+        myAnimator.SetTrigger("FadeOut");
+    }
+
+}
